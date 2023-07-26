4af8a68857138a9ac45f730d443c4eba9f2788e7
Juan Manuel Maroñas <jmmaronas@gmail.com>
2023-07-26 18:07:44 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ItemListContainer from './components/Items/ItemListContainer';
+import NavBar from './components/NavBar/NavBar';
+import ItemDetailListContainer from './components/ItemDetail/ItemDetailContinerlist';
+import FormList from './components/Form/FormList';
+
+
+const links=[
+  {
+    id:1,
+    href:"#",
+    title:"Inicio"
+  },
+  {
+    id:2,
+    href:"#",
+    title:"Categoria1"
+  },
+  {
+    id:3,
+    href:"#",
+    title:"Categoria2"
+  },
+  {
+    id:4,
+    href:"#",
+    title:"Contacto"
+  }
+]
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <BrowserRouter>
+      <NavBar links={links}/>
+      <FormList/>
+      <Routes>
+        <Route path='/' element={<ItemListContainer />}/>
+        <Route path='/product/:productId' element={<ItemDetailListContainer/>}/>
+        <Route path='/category/:categoryId' element={<ItemListContainer/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
